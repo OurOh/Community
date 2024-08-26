75546778f6ed4fe0dcd7d0a89a9bd4696d75bf7e
OurOh <ahn980321@gmail.com>
2024-08-26 16:38:42 +0900

    0826 test

---
+++ admin/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    const adminname = $("#adminform").val();
+    const adminname = $("#adminform").val();
+
+    if(adminname.trim() === "" || adminpass.trim() === ""){
+        alert("관리자 아이디 또는 비밀번호를 입력하세요.");
+    }
+});
